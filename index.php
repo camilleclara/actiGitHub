da8277a3ade2cc9c4ffc2acc1e93623aa3a3954e
Camille Berrier <c.berrier-plater@interface3.be>
2019-10-22 10:53:21 +0200

    ajout de php a l'index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GITHUB</title>
+</head>
+<body>
+    <h1>Exercice Git &amp; GitHub </h1>
+    <p>Ouvrez la console.</p>
+    <?php 
+     echo("Ceci est du php.");
+    ?>
+</body>
+</html>
